52959da30341bfec7c4ab38eccbbd56ae697bc7d
LeopoldHock <leopoldhock@outlook.com>
2021-08-31 17:10:49 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace southfury_csharp_dojo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
