0d330e3989caa540e85135245f64e612646993bf
LeopoldHock <leopoldhock@outlook.com>
2021-09-27 16:39:36 +0200

    added more exercises

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-// SOUTHFURY CODING DOJO - The RPG
+// SOUTHFURY CODING DOJO - 10/28/2021
+// --- EXERCISE 1: Randomization ---
+// Randomization is an important part of video game design. With the Random class, C#
+// offers a quick way to achieve some randomized results.
 
 namespace southfury_csharp_dojo
 {
-    class southfury_csharp_dojo
+    class Program
     {
         static void Main()
         {
