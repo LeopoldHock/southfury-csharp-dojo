92c19cc168565b3a70b393e9efcb1f8967aee584
LeopoldHock <leopoldhock@outlook.com>
2021-10-01 20:59:19 +0200

    work on the rpg

--- Program.cs
+++ Program.cs
@@ -1,17 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
-// SOUTHFURY CODING DOJO - 10/28/2021
-// --- EXERCISE 1: Randomization ---
-// Randomization is an important part of video game design. With the Random class, C#
-// offers a quick way to achieve some randomized results.
+// SOUTHFURY CODING DOJO - The RPG
 
 namespace southfury_csharp_dojo
 {
-    class Program
+    class southfury_csharp_dojo
     {
         static void Main()
         {
+            Console.WriteLine("Game starting.");
+            Player player = new Player();
+            Console.WriteLine("Welcome, " + player.name + "! Your adventure begins on the shores of Westfall...");
+            Combat combat = new Combat(player);
+        }
+    }
 
+    class Player
+    {
+        public string name;
+        public int hpTotal = 50;
+        public int hpCurrent;
+
+        public Player()
+        {
+            Console.WriteLine("Name your character..");
+            this.name = Console.ReadLine();
+            this.hpCurrent = this.hpTotal;
+        }
+    }
+
+    class Enemy
+    {
+        public Guid id;
+        public EnemyType type;
+        public int hpTotal = 25;
+        public int hpCurrent;
+
+        public Enemy()
+        {
+            this.id = Guid.NewGuid();
+            this.hpCurrent = this.hpTotal;
+            Random r = new Random();
+            int randomNumber = r.Next(3);
+            if (randomNumber == 0)
+            {
+                this.type = EnemyType.Bandit;
+            }
+            else if (randomNumber == 1)
+            {
+                this.type = EnemyType.Gnoll;
+            }
+            else
+            {
+                this.type = EnemyType.Murloc;
+            }
+        }
+    }
+
+    enum EnemyType
+    {
+        Murloc,
+        Bandit,
+        Gnoll
+    }
+
+    class Combat
+    {
+        public Player player;
+        public List<Enemy> enemies = new List<Enemy>();
+
+        public Combat(Player player)
+        {
+            this.player = player;
+            Random random = new Random();
+            int numberOfEnemies = random.Next(2, 5);
+            for (int i = 0; i < numberOfEnemies; i++)
+            {
+                Enemy enemy = new Enemy();
+                this.enemies.Add(enemy);
+            }
+            Console.WriteLine("Watch yourself! " + this.enemies.Count + " enemies appear!");
         }
     }
 }
